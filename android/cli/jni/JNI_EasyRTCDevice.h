01379448099134b915bc371e8c8566735fbae1d3
Milky <Milky@github.com>
2026-05-22 17:30:06 +0800

    delete cli

--- android/cli/jni/JNI_EasyRTCDevice.h
+++
@@ -1,9 +0,0 @@
-#ifndef __JNI_EASYRTC_DEVICE_H__
-#define __JNI_EASYRTC_DEVICE_H__
-
-#include <android/log.h>
-
-#define LOG_TAG "EasyRTC"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-
-#endif
